b61b6065370e0a40e009f76e536b8cf18476d041
Jhxnnat <JhonaDav@proton.me>
2026-04-18 14:09:46 -0500

    Fix typo

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,15 +1,15 @@
 variable "resource_group_name" {
-	descripiton = "name of resource group"
+	description = "name of resource group"
 	type        = string
 }
 
 variable "location" {
-	descripiton = "location of the resource group"
+	description = "location of the resource group"
 	type        = string
 	default     = "East US"
 }
 
 variable "storage_account_name" {
-	descripiton = "name of the storage account"
+	description = "name of the storage account"
 	type        = string
 }
